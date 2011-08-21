851f7f1904f405831223d0dfb40652563ae7af33
Justin Davis <juster@cpan.org>
2011-08-21 20:16:38 +0000

    Import of JUSTER/WWW-AUR-0.12 from CPAN.
    gitpan-cpan-distribution: WWW-AUR
    gitpan-cpan-version:      0.12
    gitpan-cpan-path:         JUSTER/WWW-AUR-0.12.tar.gz
    gitpan-cpan-author:       JUSTER
    gitpan-cpan-maturity:     released

--- lib/WWW/AUR/RPC.pm
+++ lib/WWW/AUR/RPC.pm
@@ -19,7 +19,7 @@ my %_RENAME_FOR = ( 'Description' => 'desc',
 
 #---HELPER FUNCTION---
 # Purpose: Map JSON package info keys to their new names...
-sub _rpc_pretty_pkginfo
+sub _munge_result
 {
     my ($info_ref) = @_;
 
@@ -50,11 +50,33 @@ sub info
     my $data = $json->decode( $resp->content );
 
     if ( $data->{type} eq "error" ) {
-        return undef if $data->{results} eq 'No results found';
+        return () if $data->{results} eq 'No results found';
         Carp::croak "Remote error: $data->{results}";
     }
 
-    return %{ _rpc_pretty_pkginfo( $data->{results} ) };
+    return %{ _munge_result( $data->{results} ) };
+}
+
+sub multiinfo
+{
+    my (@names) = @_;
+
+    my $uri     = rpc_uri( "multiinfo", @names );
+    my $ua      = WWW::AUR::UserAgent->new;
+    my $resp    = $ua->get( $uri );
+
+    Carp::croak 'Failed to call multiinfo AUR RPC: ' . $resp->status_line
+        unless $resp->is_success;
+
+    my $json = JSON->new;
+    my $data = $json->decode( $resp->content );
+
+    if ( $data->{type} eq "error" ) {
+        return () if $data->{results} eq 'No results found';
+        Carp::croak "Remote error: $data->{results}";
+    }
+
+    return map { _munge_result($_) } @{$data->{results}};
 }
 
 sub search
@@ -87,12 +109,12 @@ sub search
         Carp::croak "Remote error: $data->{results}";
     }
 
-    my $results = [ map { _rpc_pretty_pkginfo( $_ ) } @{ $data->{results} } ];
+    my $results = [ map { _munge_result( $_ ) } @{ $data->{results} } ];
 
     if ( $regexp ) {
         $results = [ grep { $_->{name} =~ /$regexp/ } @$results ];
     }
-    
+
     return $results;
 }
 
@@ -111,10 +133,10 @@ sub msearch
 
     if ( $json_ref->{type} eq 'error' ) {
         return [] if $json_ref->{results} eq 'No results found';
-        Carp::croak "Remote error: $json_ref->{results}";        
+        Carp::croak "Remote error: $json_ref->{results}";
     }
 
-    return [ map { _rpc_pretty_pkginfo( $_ ) } @{ $json_ref->{results} } ];
+    return [ map { _munge_result( $_ ) } @{ $json_ref->{results} } ];
 }
 
 1;
--- lib/WWW/AUR/URI.pm
+++ lib/WWW/AUR/URI.pm
@@ -12,171 +12,60 @@ use WWW::AUR qw(); # for global variables
 our @ISA         = qw(Exporter);
 our @EXPORT_OK   = qw(pkgfile_uri pkgbuild_uri pkg_uri rpc_uri);
 our %EXPORT_TAGS = ( 'all' => [ @EXPORT_OK ] );
+our $Scheme      = 'http';
+
+sub _pkgdir
+{
+    my ($pkgname) = @_;
+    my $pre = substr $pkgname, 0, 2;
+    return "packages/$pre/$pkgname";
+}
 
 sub pkgfile_uri
 {
     my ($pkgname) = @_;
-    return "http://$WWW::AUR::HOST/packages/$pkgname/$pkgname.tar.gz";
+    my $dir = _pkgdir($pkgname);
+    return "$Scheme://$WWW::AUR::HOST/$dir/$pkgname.tar.gz";
 }
 
 sub pkgbuild_uri
 {
     my ($pkgname) = @_;
-    return "http://$WWW::AUR::HOST/packages/$pkgname/PKGBUILD"
+    my $dir = _pkgdir($pkgname);
+    return "$Scheme://$WWW::AUR::HOST/$dir/PKGBUILD"
 }
 
 sub pkg_uri
 {
     my %params = @_;
     my $scheme = delete $params{'https'} ? 'https' : 'http';
+    $scheme  ||= $Scheme;
     my $uri    = URI->new( "$scheme://$WWW::AUR::HOST/packages.php" );
     $uri->query_form([ %params ]);
     return $uri->as_string;
 }
 
-my @_RPC_METHODS = qw/ search info msearch /;
+my @_RPC_METHODS = qw/ search info multiinfo msearch /;
 
 sub rpc_uri
 {
-    my ($method, $arg) = @_;
+    my $method = shift;
 
     Carp::croak( "$method is not a valid AUR RPC method" )
         unless grep { $_ eq $method } @_RPC_METHODS;
 
-    my $uri = URI->new( "http://$WWW::AUR::HOST/rpc.php" );
-    $uri->query_form( 'type' => $method, 'arg' => $arg );
+    my $uri = URI->new( "$Scheme://$WWW::AUR::HOST/rpc.php" );
+
+    my @qparms = ( 'type' => $method );
+    if ($method eq 'multiinfo') {
+        push @qparms, map { ( 'arg[]' => $_ ) } @_;
+    }
+    else {
+        push @qparms, ( 'arg' => shift );
+    }
+
+    $uri->query_form( \@qparms );
     return $uri->as_string;
 }
 
 1;
-
-__END__
-
-=head1 NAME
-
-WWW::AUR::URI - Generate dynamic URIs for accessing the AUR
-
-=head1 DESCRIPTION
-
-This is a collection of functions used internally by other modules
-in the WWW-AUR distribution. For advanced users only.
-
-=head1 EXPORTS
-
-This module exports nothing by default. You must explicitly import
-functions or import the C<all> tag to import all functions.
-
-  use WWW::AUR::URI qw( pkgfile_uri pkgbuild_uri pkg_uri rpc_uri );
-  use WWW::AUR::URI qw( :all );
-
-=head1 FUNCTIONS
-
-=head2 pkgfile_uri
-
-  $URI = pkgfile_uri( $PKGNAME )
-
-=over 4
-
-=item C<$PKGNAME>
-
-The name of the package.
-
-=item C<$URI>
-
-The URI to the source package tarball.
-
-=back
-
-=head2 pkgbuild_uri
-
-  $URI = pkgbuild_uri( $PKGNAME )
-
-=over 4
-
-=item C<$PKGNAME>
-
-The name of the package.
-
-=item C<$URI>
-
-The URI to the conveniently extracted PKGBUILD file.
-
-=back
-
-=head2 pkg_uri
-
-  $URI = pkg_uri( %QUERY_PARAMS )
-
-This generates a URI for the L<http://aur.archlinux.org/packages.php>
-webpage. The one that shows package information and comments, etc.
-
-=over 4
-
-=item C<%QUERY_PARAMS>
-
-You can supply whatever query parameters that you want. You might want
-to look at the AUR's HTML source to learn how they work.
-
-One special parameter that acts differently is the C<'https'>
-parameter. If this key exists and its value is a truthy value, then
-the URI is given as an I<https> link and not an I<http> link.  The
-C<'https'> parameter is also not passed in as a query parameter.
-
-=item C<$URI>
-
-The URI to I<packages.php> with query parameters appended.
-
-=back
-
-=head2 rpc_uri
-
-  $URI = rpc_uri( $METHOD, $ARG )
-
-Generates a URI for the L<http://aur.archlinux.org/rpc.php> page.
-
-=over 4
-
-=item C<$METHOD>
-
-The RPC "method" to use. Possible values include: C<"search">, C<"info">, or
-C<"msearch">.
-
-=item C<$ARG>
-
-The RPC "argument" to give to the "method".
-
-=item C<$URI>
-
-The URI to the rpc.php page with query parameters attached.
-
-=back
-
-=head1 SEE ALSO
-
-L<WWW::AUR>
-
-=head1 AUTHOR
-
-Justin Davis, C<< <juster at cpan dot org> >>
-
-=head1 BUGS
-
-Please email me any bugs you find. I will try to fix them as quick as I can.
-
-=head1 SUPPORT
-
-Send me an email if you have any questions or need help.
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2011 Justin Davis.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of either: the GNU General Public License as published
-by the Free Software Foundation; or the Artistic License.
-
-See http://dev.perl.org/licenses/ for more information.
-
-=cut
-
-
